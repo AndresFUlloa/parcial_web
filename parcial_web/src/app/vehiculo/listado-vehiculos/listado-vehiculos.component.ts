a90c36d9e5b6fa2b44fe17f3d562a44ca368e41c
Andres Ulloa <a.ulloar@uniandes.edu.co>
2022-11-05 10:55:43 -0500

    Punto 2

--- parcial_web/src/app/vehiculo/listado-vehiculos/listado-vehiculos.component.ts
+++ parcial_web/src/app/vehiculo/listado-vehiculos/listado-vehiculos.component.ts
@@ -10,13 +10,18 @@ import { VehiculoService } from '../vehiculo.service';
 export class ListadoVehiculosComponent implements OnInit {
 
   vehiculos: Array<Vehiculo> = [];
+  totales: Array<any> = [];
   constructor(private vehiculoService: VehiculoService) { }
 
   getVehiculos(): void{
     this.vehiculoService.getVehiculos().subscribe((vehiculos) => {
       this.vehiculos = vehiculos;
-      console.log(vehiculos);
-    })
+      let marcas = new Set(this.vehiculos.map(m => m.marca));
+      marcas.forEach((marca) => {
+          const data = {marca: marca, total:this.vehiculos.filter(v => v.marca == marca).length}
+          this.totales.push(data);
+      });
+    });
   }
 
   ngOnInit() {
