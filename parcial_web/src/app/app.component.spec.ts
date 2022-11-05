724c8ebb3a97d282efe6bad841b39dca0d8c8091
Andres Ulloa <a.ulloar@uniandes.edu.co>
2022-11-05 12:21:11 -0500

    Pruebas

--- parcial_web/src/app/app.component.spec.ts
+++ parcial_web/src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ListadoVehiculosComponent } from './vehiculo/listado-vehiculos/listado-vehiculos.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, ListadoVehiculosComponent
       ],
     }).compileComponents();
   });
@@ -22,14 +24,9 @@ describe('AppComponent', () => {
 
   it(`should have as title 'parcial_web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.componentInstance;
     expect(app.title).toEqual('parcial_web');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parcial_web app is running!');
-  });
 });
